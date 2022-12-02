dd38a712f6b732922b694e0404dd32fecdc76231
Anniina Sallinen <anniina.sallinen@gmail.com>
2022-12-02 17:13:39 +0200

    Complete 1.04 and 1.05

--- todo_app/src/main.py
+++ todo_app/src/main.py
@@ -3,12 +3,11 @@ from typing import Union
 from fastapi import FastAPI
 import uvicorn
 import os
-import logger
+from . import logger
 
 app = FastAPI()
 LOG = logger.get_logger(__name__)
 
-if __name__ == '__main__':
-    port_number = os.environ.get('PORT', 5000)
-    LOG.info(f'Server started in port {port_number}')
-    uvicorn.run('main:app', port=port_number)
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
