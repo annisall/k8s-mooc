a674e28f224e3b1379dd5ba6a920660cdd7035f6
Anniina Sallinen <anniina.sallinen@gmail.com>
2023-03-28 16:56:59 +0300

    Complete exercise 2.01

--- log_output/src/main.py
+++ log_output/src/main.py
@@ -3,6 +3,7 @@ import string
 from datetime import datetime, timezone
 import logger
 from fastapi import FastAPI
+import requests
 
 app = FastAPI()
 LOG = logger.get_logger(__name__)
@@ -11,7 +12,10 @@ LOG = logger.get_logger(__name__)
 @app.get("/")
 async def root():
     time_now = datetime.now(timezone.utc)
-    return {"message": f"{time_now}: {random_str}"}
+    resp = requests.get('http://ping-pong-scv:3456/pingpong')
+    response_text = (f"""{time_now}: {random_str}
+    {resp.text}""")
+    return response_text
 
 def generate_random_str():
     rand_list = random.choices(string.ascii_letters + string.digits + string.punctuation, k = 24)
--- ping_pong/src/main.py
+++ ping_pong/src/main.py
@@ -6,9 +6,9 @@ LOG = logger.get_logger(__name__)
 
 ping = 0
 
-@app.get("/")
+@app.get("/pingpong")
 async def root():
     global ping
-    resp_body = f"Pong {ping}"
+    resp_body = f"Ping / pongs: {ping}"
     ping = ping+1
     return resp_body
